afed71762867d2c330f1d501379ef294376e6c12
jy03078959 <258137678@qq.com>
2015-07-12 23:44:40 +0800

    初始化
    初始化

---
+++ js/view/f_main.js
@@ -0,0 +1,17 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: stoneship
+ * Email:258137678@qq.com
+ * Date: 15/7/12
+ * Time: 下午4:28
+ * To change this template use File | Settings | File Templates.
+ */
+define(function (require, exports, module) {
+    //Do setup work here
+    var config = module.config()
+    return {
+        color: "black",
+        size: "unisize"
+    }
+});
+
